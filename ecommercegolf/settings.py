56f2fd3f343d5369703428b8ec64bea7d45b12c4
Devin-Elhefe <devindfd@gmail.com>
2024-11-07 13:25:38 -0700

    further testing

--- ecommercegolf/settings.py
+++ ecommercegolf/settings.py
@@ -138,15 +138,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'ecommerce_app/static'),
 ]
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 
 # Default primary key field type
